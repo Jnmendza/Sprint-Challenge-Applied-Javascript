a27efa24296553bbfcfd992ddddc72fdf62fcad3
Jnmendza <jonatmendoza@gmail.com>
2019-09-06 09:54:49 -0700

    Cards elements built with content and classes

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+//  .then(resolve => {
+//      console.log(resolve)
+//  })
+    
+
+ const cardsContainer = document.querySelector('.cards-container')
+
+ const cardCreator = (headline, imgUrl, authorsName) => {
+     // Add elements
+     const card = document.createElement('div'),
+           headlineDiv = document.createElement('div'),
+           author = document.createElement('div'),
+           imageContainer = document.createElement('div'),
+           image = document.createElement('img'),
+           name = document.createElement('span');
+
+    // Add classes
+    card.classList.add('card');
+    headlineDiv.classList.add('headline');
+    author.classList.add('author');
+    imageContainer.classList.add('img-container');
+
+    // Add src
+    image.src = imgUrl
+
+    //Add Content
+    headlineDiv.textContent = headline
+    name.textContent = authorsName
+
+    //Append 
+    card.appendChild(headlineDiv);
+    card.appendChild(author);
+    author.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    author.appendChild(name);
+
+    return card
+ }
